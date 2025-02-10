811e353d263ddd2d9bc3c61574f0800f859d24d5
PRANESH11082004 <praneshnarayanan07@gmail.com>
2025-02-10 23:20:40 +0530

    summa

---
+++ array1.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+
+public class array1 {
+    public static void main(String[] args) {
+        int[] intarray;
+        intarray = new  int[3];
+        intarray[0]=1;
+        intarray[1]=2;
+        intarray[2]=3;
+        System.out.println("intarray elements are:" + Arrays.toString(intarray));
+    }
+}
